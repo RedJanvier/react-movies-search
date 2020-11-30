ddea3f30c8f207e17b8a449b6b17c0c0b321a575
Patience250 <brownpatience66@gmail.com>
2020-11-30 18:20:35 +0200

    feat(redux): set up redux in react app

--- movie-search/src/App.js
+++ movie-search/src/App.js
@@ -1,16 +1,27 @@
-import './App.css';
+import { Fragment } from "react";
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import listMembers from "./components/listMembers";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         Welcome to Movie Search App
-        </p>
-
-      </header>
-    </div>
-  );
+    return ( <
+        Provider store = { store } >
+        <
+        Fragment >
+        <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        p > Welcome to Movie Search App < /p> <
+        /header> <
+        /div> <
+        listMembers / >
+        <
+        /Fragment> <
+        /Provider>
+    );
 }
 
 export default App;
